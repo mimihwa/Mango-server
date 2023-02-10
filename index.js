32708139f4fbf01d57e367ffe8fd86b7620d909f
mimihwa <wmfskf@naver.com>
2023-02-10 11:28:52 +0900

    index 이동

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/* function hello(){
-
-}
-export default hello;
-
-import hello from './hello.js' */
-
-/* function hello(){
-
-}
-module.exports = hello;
-
-var hello= require('./hello.js')
-hello();
- */
-
 let http=require('http');
 let hostname='127.0.0.1';
 let port='8080';
@@ -44,3 +28,137 @@ const server=http.createServer(function(req, res){
 //서버를 요청하면 대기상태로 만들어야 함(port, hostname)
 server.listen(port, hostname);  //요청
 console.log('server on');
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+         cb(null, "uploads");
+      },
+      filename: function (req, file, cb) {
+         cb(null, file.originalname)
+      },
+   }), 
+});
+
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.get('/banners',(req, res) => {
+   models.Banner.findAll({
+      limit:2
+   }).then((result)=>{
+      res.send({
+         banners:result
+      })
+   }).catch((error)=>{
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.")
+   })
+})
+
+
+app.get("/products", function (req, res) {
+   models.Product.findAll({
+      order: [["createdAt", "DESC"]],
+      attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt","soldout"],
+   })
+      .then((result) => {
+         res.send({
+            product: result,
+         });
+      })
+      .catch((err) => {
+         console.error(err);
+         res.status(400).send("에러발생");
+      });
+});
+
+app.get("/products/:id", function (req, res) {
+   const params = req.params;
+   const { id } = params;
+   models.Product.findOne({
+      where: {
+         id,
+      },
+   })
+      .then((result) => {
+         res.send({ product: result });
+      })
+      .catch((err) => {
+         console.error(err);
+         res.status(400).send("상품조회시 에러가 발생했습니다.");
+      });
+});
+
+app.post("/products", function (req, res) {
+   const body = req.body;
+   const { name, description, price, seller, imageUrl} = body;
+   if (!name || !description || !price || !seller) {
+      res.send("모든 필드를 입력해주세요");
+   }
+   models.Product.create({
+      name,
+      description,
+      price,
+      seller,
+      imageUrl
+   })
+      .then((result) => {
+         res.send({
+            product:result
+         })
+      })
+      .catch((err) => {
+         console.error(err);
+         res.status(400).send("상품업로드에 문제가 발생했습니다");
+      });
+});
+app.post("/purchase/:id", (req, res) => {
+   const {id} =req.params;
+   models.Product.update(
+      {
+         soldout:1,
+      },
+      {
+         where: {
+            id,
+         }
+      }
+   ).then((result) => {
+      res.send({
+         result:true,
+      })
+   })
+   .catch((err) => {
+      console.error(err);
+      res.status(500).send("에러가 발생했습니다");
+   });
+})
+
+app.post("/image", upload.single("image"), (req, res) => {
+   const file = req.file;
+   console.log(file);
+   res.send({
+      imageUrl: file.path,
+   });
+});
+
+app.listen(port, () => {
+   console.log("망고샵 서버 실행중");
+   models.sequelize
+      .sync()
+      .then(() => {
+         console.log("DB 연결 성공");
+      })
+      .catch((err) => {
+         console.log("DB 연결 실패");
+         console.error(err);
+         process.exit();
+      });
+});
