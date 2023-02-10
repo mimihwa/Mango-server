b35551e979925eee70c1f937d1f323800ee80865
mimihwa <wmfskf@naver.com>
2023-02-10 12:00:54 +0900

    server

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-let http=require('http');
-let hostname='127.0.0.1';
-let port='8080';
-
-//서버생성(요청:request, 응답:responsive)
-const server=http.createServer(function(req, res){
-    const path=req.url;
-    const method=req.method;
-    if(path==='/products'){
-        if(method==='GET'){
-            res.writeHead(200, {'Content-Type':'application/json'});
-            const products=JSON.stringify([
-                {
-                    name:'배변패드',
-                    price:5000,
-                }
-            ]);
-            res.end(products);
-        }else if(method==='POST'){
-            res.end('생성되었습니다.');
-        }
-    }else{
-        res.end('Good bye.');
-    }
-    
-});
-
-//서버를 요청하면 대기상태로 만들어야 함(port, hostname)
-server.listen(port, hostname);  //요청
-console.log('server on');
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -78,6 +47,7 @@ app.get("/products", function (req, res) {
          res.status(400).send("에러발생");
       });
 });
+let port='8080';
 
 app.get("/products/:id", function (req, res) {
    const params = req.params;
